c9fac4fe92e56309f8aa90a4b4729208a3f0319f
Faniry Nambinintsoa <nambinintsoafaniry@gmail.com>
2025-04-15 13:36:17 +0400

    Ajout audi, et autres modifs supplementaires

--- Pages/footage/footage.php
+++ Pages/footage/footage.php
@@ -5,46 +5,78 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>footage</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="http://localhost/supercar_project/Pages/footage/footage.css" rel="stylesheet"> -->
+    <link href="http://localhost/supercar_project/librairiecss/all.css" rel="stylesheet">
+<style>
+    footer{
+    font-size:10px; 
+}
 
-    <style>
-        footer{
-            font-size:10px;
-            
-        }
+.f1 {
+    font-size: 20px;
+    color: #939393;
+}
 
-        .f1 {
-            font-size: 20px;
-            color: #939393;
-        }
+.p1{
+    margin-left:1500px;
+    font-size:15px;
+}
 
-        .imgf1, .imgf2 {
-            width: 100px;
-            max-width: 100%; /* S'assure que l'image ne dépasse pas */
-        }
+.btn1{
+    font-size:10px;
+}
 
-        .imgf2 {
-            width: 150px;
-        }
+.image {
+    left: 12px;
+    position: absolute;
+    width: 80px;
+}
 
-        .p1{
-            margin-left:1500px;
-            font-size:15px;
-        }
+.conteneur {
+    display: flex;
+    position: relative;
+    height: 13vh;
+}
 
-        .btn1{
-            font-size:10px;
-        }
-    </style>
+.logo {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: flex;
+    gap: 20px;
+    font-size: 30px;
+}
+
+.logoinstagram {
+    color: rgb(250 153 84);
+}
+
+.logowhatsapp {
+    color: rgb(13 253 14);
+}
+
+.logoemail {
+    color: white;
+}
+
+</style>
 </head>
 <body>
 
-    <footer class="bg-dark text-white py-3 w-100">
-        <img class="imgf1" src="/supercar_project/assets/images/barre-logo.PNG">
-        <img class="imgf2" src="/supercar_project/assets/images/Réseaux.PNG">
-        <p class="p1">
-         supercar@gmail.mu
-    </p>
-    <hr>
+    <footer class="bg-dark text-white py-3 w-100">     
+  
+    <div class="conteneur">
+            <img src="/supercar_project/assets/images/barre-logo.PNG" class="image">
+        <div class="logo">
+            <a href="" alt="facebook" title="facebook" class="logofacebook"><i class="fa-brands fa-facebook"></i></a>
+            <a href="" alt="instagram" title="instagram" class="logoinstagram"><i class="fa-brands fa-instagram"></i></a>
+            <a href="" alt="whatsapp" title="whatsapp" class="logowhatsapp"><i class="fa-brands fa-whatsapp"></i></a>
+            <a href="" alt="mail" title="mail" class="logoemail"><i class="fa-regular fa-envelope"></i></a>
+        </div>
+    </div> 
+    
+        <hr>
         <center>© 2025 SuperCar - Tous droits réservés</center>
     </footer>
 
